70bfe6b9fabc2001ecab9ad2e8a3c546ef46b697
Nihita K <nihitakolukula@gmail.com>
2025-06-02 10:02:15 +0530

    db/initial

---
+++ server/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+const app = express();
+
+//config the env file (to access data)
+dotenv.config();
+
+//connection
+connectDB();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//routes
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+// port connect
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
